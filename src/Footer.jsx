42dc78ffbdd1115979ed59e79aabc4bf9aadfa00
Dom13377 <streif.dominic@gmail.com>
2024-09-03 09:59:35 -0500

    Changed anchor tags to Link tags and css styled them

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,18 +1,29 @@
 import './Footer.css'
+import { Link } from 'react-router-dom'
 
 function Footer() {
     return (
         <footer>
-        <p id="email">
-                Email: 
-        <a href="mailto:streif.dominic@gmail.com"> streif.dominic@gmail.com </a>
-                LinkedIn: 
-        <a href="https://www.linkedin.com/in/dominic-streif/"> dominic-streif </a>
+        <p id="contacts">
+                    Email: 
+            <Link className="contact-link" reloadDocument to="mailto:streif.dominic@gmail.com"> 
+                    streif.dominic@gmail.com 
+            </Link>
+                    LinkedIn: 
+            <Link className="contact-link" reloadDocument to="https://www.linkedin.com/in/dominic-streif/"> 
+                    dominic-streif 
+            </Link>
         </p>
-        <p className="credits"> Made with
-            <a href="https://vitejs.dev/"> Vite </a>
-             + 
-            <a href="https://react.dev/"> React </a>
+
+        <p className="credits"> 
+                    Made with
+            <Link className="credits-link" reloadDocument to="https://vitejs.dev/"> 
+                    Vite
+            </Link>
+                    +
+            <Link className="credits-link" reloadDocument to="https://react.dev/"> 
+                    React
+            </Link>
              
         </p>
         </footer>
