73c548f354d8ead1a48de8c6a887c424de9a076c
Dom13377 <streif.dominic@gmail.com>
2024-09-03 17:56:22 -0500

    Made more robust configurations for accessing the pdf. Also embeded google docs so that live updates will be reflected on the website

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -1,7 +1,7 @@
 import './Resume.css';
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import resume_pdf from "./assets/resume.pdf"
+import { Link } from 'react-router-dom';
 
 function Resume() {
     return (
@@ -10,7 +10,19 @@ function Resume() {
 
             <div id="parent">
 
-                <object className="pdf" data={resume_pdf}/>
+                <object className="pdf" data="https://docs.google.com/document/d/1PolR2TZ2WTlrCwjUfIAfNxmgGSQdx-FnxpmOL051P9k/preview?format=pdf">
+                    <p>
+                        No PDF Viewer available, view in
+                        <Link reloadDocument style={{"padding-left": "6px"}} to="https://docs.google.com/document/d/1PolR2TZ2WTlrCwjUfIAfNxmgGSQdx-FnxpmOL051P9k/view?usp=sharing">
+                            docs
+                        </Link>
+                    </p>
+                </object>
+                <p style={{"padding-top": "30px", "font-size": "18px"}}>
+                    <Link reloadDocument style={{"padding-left": "6px"}} to="https://docs.google.com/document/d/1PolR2TZ2WTlrCwjUfIAfNxmgGSQdx-FnxpmOL051P9k/export?format=pdf" >
+                        Download
+                    </Link>
+                </p>
 
             </div>
 
