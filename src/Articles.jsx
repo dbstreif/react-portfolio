d7022eca4c2bfc798d98d06056c1c61e072ac73f
Dominic Streif <streif.dominic@gmail.com>
2025-06-18 01:23:34 -0700

    Added projects and articles carousel

--- src/Articles.jsx
+++ src/Articles.jsx
@@ -1,19 +1,51 @@
 import './Articles.css';
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
-function Resume() {
+const Articles = () => {
+  const articles = [
+    { name: 'heartbeat_style_gossip', url: 'https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7292289305123663872' },
+    { name: 'heartbeat_style_gossip2', url: 'https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7292289305123663872' },
+  ];
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true,
+    focusOnSelect: true,
+    adaptiveHeight: true,
+  };
+
     return (
         <>
-            <Navbar/>
-
-            <div id="parent">
-                <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7292289305123663872" height="598" width="504" frameBorder="0" allowfullscreen="" title="Embedded post"></iframe>
-            </div>
-
-            <Footer/>
+        <Navbar/>
+        <div style={{ width: '60%', margin: '0 auto' }}>
+          <Slider {...settings}>
+            {articles.map((article) => (
+              <div key={article.name}>
+                <iframe 
+                    src={article.url} 
+                    height="598" 
+                    width="500"
+                    frameBorder="0" 
+                    allowFullScreen 
+                    title={article.name}
+                    style={{ border: 'none', borderRadius: '8px' }}  // Optional: border radius for aesthetics
+                ></iframe>
+              </div>
+            ))}
+          </Slider>
+        </div>
+        <Footer/>
         </>
-    )
-}
+        );
+};
 
-export default Resume;
+export default Articles;
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,17 +1,60 @@
 import './Projects.css'
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
-function Projects() {
+
+const RepoEmbed = ({ repoUrl, repoName }) => {
+    const cardUrl = `https://gh-card.dev/repos/dbstreif/${ repoName }.png?fullname=`
+  return (
+    <div className="repo-card">
+        <a href={repoUrl} target="_blank" rel="noopener noreferrer">
+            <img src={cardUrl} alt={`GitHub card for dbstreif/${repoName}`} />
+        </a>
+    </div>
+  );
+};
+
+const Projects = () => {
+  const repos = [
+    { name: 'SpaceX-API-PY', url: 'https://github.com/dbstreif/SpaceX-API-PY' },
+    { name: 'RoomieAI', url: 'https://github.com/dbstreif/RoomieAI' },
+    { name: 'Giggity', url: 'https://github.com/dbstreif/Giggity' },
+    { name: 'gamehackingtemplate', url: 'https://github.com/dbstreif/gamehackingtemplate' },
+    { name: 'heartbeatstyle-gossip', url: 'https://github.com/dbstreif/heartbeatstyle-gossip' },
+    { name: 'RougeCompany', url: 'https://github.com/dbstreif/RogueCompany' },
+    { name: 'ChristmasPI', url: 'https://github.com/dbstreif/ChristmasPI' },
+  ];
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
     return (
         <>
         <Navbar/>
-
-        <h1> Page Under Construction! </h1>
-
+        <div style={{ width: '40%', margin: '0 auto', paddingTop: '300px', paddingBottom: '300px' }}>
+          <Slider {...settings}>
+            {repos.map((repo) => (
+              <div key={repo.name}>
+                <a href={repo.url} target="_blank" rel="noopener noreferrer">
+                  <h3>{repo.name}</h3>
+                    <RepoEmbed repoUrl={repo.url} repoName={repo.name}/>
+                </a>
+              </div>
+            ))}
+          </Slider>
+        </div>
         <Footer/>
         </>
-    )
-}
+        );
+    };
 
 export default Projects;
